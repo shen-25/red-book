d438d70afebf42d5f4ca2c234dbd73b3253a2629
shen-25 <3553642457@qq.com>
2022-12-30 14:53:12 +0800

    '构建工程成功'

---
+++ book-api/src/main/java/com/imooc/Application.java
@@ -0,0 +1,10 @@
+package com.imooc;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class Application {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
